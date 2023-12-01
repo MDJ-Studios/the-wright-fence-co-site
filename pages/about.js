e501e746e52f25978bca9fccdb226aa2029b5052
Daniel Scott <daniel@mydeveloperjourney.com>
2023-12-01 12:38:57 -0600

    wip: updates title and adds mising semicolon/quotes

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,18 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function About() {
-  return (
-    <>
-      <Head>
-        <title>About</title>
-      </Head>
-      <main>
-        <div>
-          <p>
-            This is the about page
-          </p>
-        </div>
-      </main>
-    </>
-  )
+	return (
+		<>
+			<Head>
+				<title>About | The Wright Fence Co.</title>
+			</Head>
+			<main>
+				<div>
+					<p>
+						This is the about page
+					</p>
+				</div>
+			</main>
+		</>
+	);
 }
