5ca789c493bc5eb1b737445b55a6bef7e2686f8b
Daniel Scott <daniel@mydeveloperjourney.com>
2023-12-01 12:38:31 -0600

    wip: updates to double quotes and adds missing ;

--- pages/fence-types.js
+++ pages/fence-types.js
@@ -1,18 +1,18 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function FenceTypes() {
     return (
         <>
-        <Head>
-            <title>Fence Types</title>
-        </Head>
-        <main>
-            <div>
-            <p>
-                This is the fence types page
-            </p>
-            </div>
-        </main>
+            <Head>
+                <title>Fence Types | The Wright Fence Co.</title>
+            </Head>
+            <main>
+                <div>
+                    <p>
+                        This is the fence types page
+                    </p>
+                </div>
+            </main>
         </>
-    )
+    );
 }
