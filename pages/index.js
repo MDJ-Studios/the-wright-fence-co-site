0a8d348cb46fb834c9b9c8398fbe27247816b83a
Clue355 <dm.carlosluevano@gmail.com>
2023-12-06 18:11:41 -0600

    feat: added reviews sections and small fix to tablet view

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import ServiceOffering from "@/components/main-page/service-offering/";
 import Carousel from "@/components/hero-carousel/";
 import Contact from "@/components/main-page/contact-us";
 import Layout from "@/components/Layout";
+import Reviews from "@/components/main-page/review-section";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function Home() {
                 <main className={`${styles.main} ${inter.className}`}>
                     <Carousel slides={slides} />
                     <ServiceOffering />
+                    <Reviews />
                     <Contact />
                 </main>
             </Layout>
